c77fa8def08bcb47f996bbf440d45ca025c37c27
kirtigalani <kittugalani@gmail.com>
2024-12-04 18:56:16 +0530

    Add project files.

---
+++ Diff m overriding hidding/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Diff_m_overriding_hidding
+{
+    public class BaseClass
+    {
+        public virtual void print()
+        {
+            Console.WriteLine("I am Base class");
+        }
+    }
+    public class derived : BaseClass
+    {
+        public new void print()
+        {
+            Console.WriteLine("I am Derive class");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass B = new derived();
+            B.print();
+
+            derived D = new derived();
+            D.print();
+        }
+    }
+}
